2a4599c4274c3598bf4086b11a9615e7f7da29c3
Afsanefda <afsoonfadaei@gmail.com>
2020-12-25 23:49:14 +0330

    :present: extra code removed

--- hotel-client/cypress/integration/Home/home.spec.js
+++ hotel-client/cypress/integration/Home/home.spec.js
@@ -2,8 +2,4 @@ describe('Home', () => {
   it('should renders home', () => {
     cy.visit(Cypress.env('BASE_URL'));
   });
-
-  it('should renders home', () => {
-    expect(true).to.be.false
-  });
 });
